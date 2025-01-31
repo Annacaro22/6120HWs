47f36d526d8109427dc600f890df7887fb67afa0
Annacaro22 <acb365@cornell.edu>
2025-01-30 20:58:07 -0500

    upload all files

--- blocksandcfg.py
+++ blocksandcfg.py
@@ -3,55 +3,114 @@ import json
 
 def main():
     program = json.load(sys.stdin)
-    print("basic blocks: ")
-    blocks = basicblocks(program)
-    print(blocks)
-    print("cfg: ")
-    cfg = cfg(blocks)
-    print(cfg)
+    print()
 
-
-
-def basicblocks(program):
-    return []
-    blocks = []
 
     functions = (list(program.values())[0])
     func = 0
     while func < len(functions):
-        instrs = (list(program.values())[0])[0]
-        instructions = (list(instrs.values())[0])
-        i = 0
-        while i < len(instructions): #for inst in instructions
-            currdict = instructions[i] #currdict is the current instruction       
-            if currdict.get("op") == "jmp":
-                newblock = [] #add instruction that jump points to to the new block
-                blocks.append(newblock)
-            elif currdict.get("op") == "br":
-                newblock = [] # add instruction 1 that branch points to to the new block
-                newblock2 = [] # add instruction 2 that branch points to to the new block
-                blocks.apend(newblock)
-                blocks.append(newblock2)
-            blocks[i].append(currdict) #i don't think the i is right here bc the index of the instruction isn't necessarily the number of the block
-            #maybe keep a currblock counter?? and update accordingly?
-            i+=1
+        onefunc = functions[func]
+        print("\n Basic Blocks in function " + onefunc.get("name") + " are: ")
+        blocklabel = basicblocks(onefunc)
+        print("\n labels to blocks: ")
+        print(blocklabel[1])
+
+        print("\n CFG in function " + onefunc.get("name") + " is: ")
+        print(getcfg(blocklabel[0],blocklabel[1]))        
+
         func+=1
 
-    return blocks
 
 
+#jump, branch, call, label(no jump just continue)
+def basicblocks(onefunc):
+    blocks = []
+    labelstoblock = {}
+    currlabel = [False, ""]
+    instructions = onefunc.get("instrs")
+    i = 0
+    newblock = []
+    nolabel = 0
+    while i < len(instructions): #for inst in instructions
+        currdict = instructions[i] #currdict is the current instruction 
 
-
-def cfg(blocks):
-    return {}
-    cfg = {}
+        if "label" in currdict.keys():
+            if newblock != []: #since we divide blocks after terminators and after labels, if you have
+            #a terminator followed by a label, you get an empty block, which we don't need to store.
+                blocks.append(newblock)
+                if currlabel[0] == True: #curr block has a label
+                    labelstoblock[currlabel[1]] = newblock
+                elif nolabel == 0: # first block "start"
+                    labelstoblock["start"] = newblock
+                    nolabel+=1
+                else: #other nonlabelled block
+                    labelstoblock["nolabel" + str(nolabel)] = newblock
+                    nolabel+=1
+                print("\n")
+                print(newblock)
+            newblock = []
+            currlabel = (True, currdict.get("label"))
+        else:
+            newblock.append(currdict)
+            if currdict.get("op") == "jump" or currdict.get("op") == "br":
+                blocks.append(newblock)
+                if currlabel[0] == True: #curr block has a label
+                    labelstoblock[currlabel[1]] = newblock
+                elif nolabel == 0: #first blcok "start"
+                    labelstoblock["start"] = newblock
+                    nolabel+=1
+                else: #other nonlabelled block
+                    labelstoblock["nolabel" + str(nolabel)] = newblock
+                    nolabel+=1
+                print("\n")
+                print(newblock)
+                newblock = []
+                currlabel = (False, "")
+        i+=1
     
+    blocks.append(newblock)
+    if currlabel[0] == True: #current block has a label
+        labelstoblock[currlabel[1]] = newblock
+    elif nolabel == 0: #first block "start"
+        labelstoblock["start"] = newblock
+        nolabel+=1
+    else: #other nonlabelled block
+        labelstoblock["nolabel" + str(nolabel)] = newblock
+        nolabel+=1
+    print("\n")
+    print(newblock)
+
+    return [blocks, labelstoblock]
+
+
+
+
+def getcfg(blocks, labelstoblock):
+
+    cfg = {}
     for block in blocks:
-        if block[-1].get("op") == "jmp":
-            cfg[block] = #the instruction at whatever x jumps to
-        if block[-1].get("op") == "br":
-            cfg[block] = #the instructions at whatever x, y branch to
-        if block[-1].get("op") == "label":
-            cfg[block] = #the next instruction, bc if we've hit a label then that means we're just progressing as normal
+        lastinstr = block[-1] #last instruction of block
+        for key, val in labelstoblock.items(): #this is just to get the label of current block
+            if val == block:
+                currlabel = key
+        if lastinstr.get("op") == "jmp":
+            cfg[currlabel] = [lastinstr.get("labels")[0]] #only one child block
+        elif lastinstr.get("op") == "br":
+            cfg[currlabel] = [lastinstr.get("labels")[0]]
+            cfg[currlabel].append(lastinstr.get("labels")[1]) #2 children blocks
+        elif lastinstr.get("op") == "ret":
+            cfg[currlabel] = ["end"] #return goes to end
+        elif blocks.index(block) < len(blocks)-1:
+            cfg[currlabel] = [blocks[blocks.index(block)+1]] #falls through to next block
+
+    cfg[currlabel] = ["end"]  #last block goes to end
 
     return cfg
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
--- minibrilprogram.py
+++ minibrilprogram.py
@@ -1,26 +1,31 @@
-#ARGS: the positions.json file in bril/test/parse, should have 1 jump
+#ARGS: my benchmark, combination.json, which should return 16-- 4 branches, 7 labels, 5 function calls, and 0 jumps.
 
 import sys
 import json
 
 
-#Program to count the number of branching commands; the number of times br and jmp are called. Alternatively,
-#the number of arrows in a CFG
+#Program to count the number of significant commands; the number of times br, jmp, or function call are called, or
+#a label appears.
 
 count = 0
 program = json.load(sys.stdin)
 functions = (list(program.values())[0])
+#print(functions)
 func = 0
 while func < len(functions):
-    instrs = (list(program.values())[0])[0]
-    instructions = (list(instrs.values())[0])
+    onefunc = functions[func]
+    #onefunc = (list(program.values())[0])[0]
+    #print(onefunc)
+    instructions = onefunc.get("instrs")
+    #instructions = (list(instrs.values())[0])
+    #print(instructions)
     i = 0
     while i < len(instructions):
         currdict = instructions[i]
-        if currdict.get("op") == "jmp":
+        if "label" in currdict.keys():
             count+=1
-        if currdict.get("op") == "br":
-            count+=2 #add two because branch opens up two new possible paths
+        elif currdict.get("op") == "call" or currdict.get("op") == "jmp" or currdict.get("op") == "br":
+            count+=1
         i+=1
     func+=1
 
